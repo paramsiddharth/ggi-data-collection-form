4bb5663c9336b40c51d6d83fe667435fac248369
Param Siddharth <contact@paramsid.com>
2023-02-19 15:52:03 +0530

    Finish form front-end
    Signed-off-by: Param Siddharth <contact@paramsid.com>

---
+++ src/components/layout.jsx
@@ -0,0 +1,35 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+import styles from '@/styles/Home.module.css';
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Head>
+        <meta name="description" content="A form to collect data." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.description}>
+          <div className={styles.header}>
+            <h1>Form</h1>
+          </div>
+          <div className={styles.menu}>
+            <Link href='#' className={styles.item}>Dashboard</Link>
+            <Link href='#' className={styles.item}>About</Link>
+          </div>
+        </div>
+
+        <div className={styles.center}>
+          { children }
+        </div>
+
+        <div className={styles.footer}>
+          &copy; Param Siddharth 2023-{new Date().getFullYear()}
+        </div>
+      </main>
+    </>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { useRef, useState } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import {
@@ -14,8 +15,8 @@ import {
   Typography
 } from '@mui/material';
 
+import Layout from '@/components/layout';
 import styles from '@/styles/Home.module.css';
-import { useState } from 'react';
 
 export default function Home() {
   const defaultValues = {
@@ -32,129 +33,203 @@ export default function Home() {
     ]
   };
 
-  const [positions, setPositions] = useState([]);
-  const [resumé, setResumé] = useState(null);
+  const [error, setError] = useState(null);
+  const errorRef          = useRef();
+
+  const [name, setName]                           = useState('');
+  const [email, setEmail]                         = useState('');
+  const [age, setAge]                             = useState(null);
+  const [phone, setPhone]                         = useState('');
+  const [inst, setInst]                           = useState('');
+  const [years, setYears]                         = useState(defaultValues.years);
+  const [positions, setPositions]                 = useState([]);
+  const [whatMakesYouIdeal, setWhatMakesYouIdeal] = useState('');
+  const [resumé, setResumé]                       = useState(null);
+  const [queries, setQueries]                     = useState('');
+
+  const nameRef              = useRef();
+  const emailRef             = useRef();
+  const ageRef               = useRef();
+  const phoneRef             = useRef();
+  const instRef              = useRef();
+  const yearsRef             = useRef();
+  const positionsRef         = useRef();
+  const whatMakesYouIdealRef = useRef();
+  const resuméRef            = useRef();
+  const queriesRef           = useRef();
+
+  const onChange = set => e => set(e.target.value);
+  /* if (!emailInput.current.checkValidity()) {
+    emailInput.current.reportValidity();
+  } */
+
+  const submit = e => {
+    const defaultValidation = [
+      nameRef,
+      emailRef,
+      ageRef,
+      phoneRef,
+      instRef,
+      positionsRef,
+      whatMakesYouIdealRef,
+      resuméRef
+    ];
+
+    let valid = true;
+
+    for (const ref of defaultValidation) {
+      if (!ref.current.checkValidity()) {
+        ref.current.reportValidity();
+        valid = false;
+      }
+    }
+
+    return valid;
+  };
 
   return (
-    <>
+    <Layout>
       <Head>
         <title>Form</title>
-        <meta name="description" content="A form to collect data." />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <div className={styles.header}>
-            <h1>Form</h1>
-          </div>
-          <div className={styles.menu}>
-            <Link href='#' className={styles.item}>Dashboard</Link>
-            <Link href='#' className={styles.item}>About</Link>
-          </div>
+      <form action='/api/submit' method='post' onSubmit={submit}>
+        <div className={styles.input}>
+          <Typography variant='h2' align='center'>Form</Typography>
         </div>
-
-        <div className={styles.center}>
-          <form>
-            <div className={styles.input}>
-              <Typography variant='h2' align='center'>Form</Typography>
-            </div>
-            <div className={styles.input}>
-              <TextField fullWidth variant='outlined' label='Name' required />
-            </div>
-            <div className={styles.input}>
-              <TextField fullWidth variant='outlined' label='E-mail address' type='email' required />
-            </div>
-            <div className={styles.input}>
-              <TextField fullWidth variant='outlined' label='Age' type='number' required />
-            </div>
-            <div className={styles.input}>
-              <TextField fullWidth variant='outlined' label='Phone number' type='tel' required />
-            </div>
-            <div className={styles.input}>
-              <TextField fullWidth variant='outlined' label='Academic institution' required />
-            </div>
-            <div className={styles.input}>
-              <InputLabel>Years of experience *</InputLabel>
-              <Slider
-                type='range'
-                defaultValue={defaultValues.years}
-                min={1}
-                max={10}
-                step={1}
-                marks={Array.from(Array(10).keys()).map(k => ({ value: k + 1, label: k + 1 }))}
-              />
-            </div>
-            <FormControl sx={{ width: '100%' }} className={styles.input}>
-              <InputLabel id='posofint' mt={3}>Positions of interest *</InputLabel>
-              <Select
-                labelId='posofint'
-                multiple
-                value={positions}
-                onChange={({ target: { value } }) => {
-                  setPositions(
-                    typeof value === 'string' ? value.split(',') : value
-                  );
-                }}
-                input={<OutlinedInput label="Positions of interest *" />}
-                renderValue={(selected) => selected.join(', ')}
-              >
-                {defaultValues.positions.map(name => (
-                  <MenuItem key={name} value={name}>
-                    <Checkbox checked={positions.indexOf(name) > -1} />
-                    <ListItemText primary={name} />
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-            <div className={styles.input}>
-              <TextField
-                fullWidth variant='outlined' multiline minRows={3}
-                label='What makes you an ideal candidate?' required />
-            </div>
-            <div className={styles.input}>
-              <Button type='button' onClick={e => {
-                if (resumé != null) {
-                  e.preventDefault();
-                  setResumé(null);
-                }
-              }} variant='contained' component='label'
-              fullWidth
-              color='info'
-              // sx={{ textTransform: 'none' }}
-              >
-                {
-                  resumé == null ?
-                    'Upload resumé' :
-                    'Clear resumé'
-                } *
-                <input onChange={({ target: { files: [file] } }) => {
-                  console.log(file);
-                  setResumé(file);
-                }} hidden accept='.pdf,.doc,.docx,.odf' type='file' />
-              </Button>
-              { ' ' }
-              { resumé?.name }
-            </div>
-            <div className={styles.input}>
-              <TextField
-                fullWidth variant='outlined' multiline minRows={3}
-                label='Queries, if any' />
-            </div>
-            <Button type='submit'
-              fullWidth
-              variant='contained'
-              color='success'
-            >
-              Submit
-            </Button>
-          </form>
+        <div className={styles.input}>
+          <TextField fullWidth variant='outlined' label='Name' required 
+            inputRef={nameRef}
+            value={name}
+            onChange={onChange(setName)}
+          />
         </div>
-
-        <div className={styles.footer}>
-          &copy; Param Siddharth 2023-{new Date().getFullYear()}
+        <div className={styles.input}>
+          <TextField fullWidth variant='outlined' label='E-mail address' type='email' required 
+            inputRef={emailRef}
+            value={email}
+            onChange={onChange(setEmail)}
+          />
         </div>
-      </main>
-    </>
+        <div className={styles.input}>
+          <TextField fullWidth variant='outlined' label='Age' type='number' required 
+            inputRef={ageRef}
+            value={age}
+            onChange={onChange(setAge)}
+          />
+        </div>
+        <div className={styles.input}>
+          <TextField fullWidth variant='outlined' label='Phone number' type='tel' required 
+            inputRef={phoneRef}
+            value={phone}
+            onChange={onChange(setPhone)}
+          />
+        </div>
+        <div className={styles.input}>
+          <TextField fullWidth variant='outlined' label='Academic institution' required 
+            inputRef={instRef}
+            value={inst}
+            onChange={onChange(setInst)}
+          />
+        </div>
+        <div className={styles.input}>
+          <InputLabel>Years of experience *</InputLabel>
+          <Slider
+            type='range'
+            ref={yearsRef}
+            defaultValue={defaultValues.years}
+            value={years}
+            onChange={onChange(setYears)}
+            min={1}
+            max={10}
+            step={1}
+            marks={Array.from(Array(10).keys()).map(k => ({ value: k + 1, label: k + 1 }))}
+          />
+        </div>
+        <FormControl sx={{ width: '100%' }} className={styles.input}>
+          <InputLabel id='posofint' mt={3}>Positions of interest *</InputLabel>
+          <Select
+            inputRef={positionsRef}
+            required
+            labelId='posofint'
+            multiple
+            value={positions}
+            onChange={({ target: { value } }) => {
+              setPositions(
+                typeof value === 'string' ?
+                  value.split(',') : // Autofill
+                  value
+              );
+            }}
+            input={<OutlinedInput label="Positions of interest *" />}
+            renderValue={(selected) => selected.join(', ')}
+          >
+            {defaultValues.positions.map(name => (
+              <MenuItem key={name} value={name}>
+                <Checkbox checked={positions.indexOf(name) > -1} />
+                <ListItemText primary={name} />
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <div className={styles.input}>
+          <TextField
+            fullWidth variant='outlined' multiline minRows={3}
+            label='What makes you an ideal candidate?' required 
+            inputRef={whatMakesYouIdealRef}
+            value={whatMakesYouIdeal}
+            onChange={onChange(setWhatMakesYouIdeal)}
+          />
+        </div>
+        <div className={styles.input}>
+          <Button type='button' onClick={e => {
+            if (resumé != null) {
+              e.preventDefault();
+              setResumé(null);
+            }
+          }} variant='contained' component='label'
+          /* fullWidth */
+          color='info'
+            // sx={{ textTransform: 'none' }}
+          >
+            {
+              resumé == null ?
+                'Upload resumé' :
+                'Clear resumé'
+            } *
+            <input ref={resuméRef}
+              onChange={({ target: { files: [file] } }) => setResumé(file)}
+              className={styles.hiddenFileInput} required accept='.pdf,.doc,.docx,.odf' type='file' 
+            />
+          </Button>
+          { ' ' }
+          { resumé?.name }
+        </div>
+        <div className={styles.input}>
+          <TextField
+            fullWidth variant='outlined' multiline minRows={3}
+            label='Queries, if any' 
+            inputRef={queriesRef}
+            value={queries}
+            onChange={onChange(setQueries)}
+          />
+        </div>
+        {
+          error == null
+            ? '' :
+            <div className={styles.input}>
+              <Typography color='red' fontWeight='bold' ref={errorRef}>
+                { error }
+              </Typography>
+            </div>
+        }
+        <Button type='submit'
+          fullWidth
+          variant='contained'
+          color='success'
+        >
+          Submit
+        </Button>
+      </form>
+    </Layout>
   );
 }
